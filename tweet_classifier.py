1b5f8c6b181039a872e51150be37d3bd4634316d
Digvijay Rawat <56080380+diggee@users.noreply.github.com>
2020-03-20 10:31:38 +0530

    made the code modular

--- tweet_classifier.py
+++ tweet_classifier.py
@@ -22,65 +22,58 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import cross_val_score
-import time
+from bayes_opt import BayesianOptimization
+import warnings
+warnings.filterwarnings('ignore')
 
 #%% reading data
 
 def get_data():
-    full_train_data = pd.read_csv('train.csv', index_col = 'id')
-    full_test_data = pd.read_csv('test.csv', index_col = 'id')
+    full_train_data = pd.read_csv('train.csv', index_col = 'id', usecols = ['id','text','target'])
+    full_test_data = pd.read_csv('test.csv', index_col = 'id', usecols = ['id','text'])
+    # there are no null values in either the train set or the test set.
+    print('Total null values in the train set - '+str(full_train_data.isnull().sum().sum()))
+    print('Total null values in the test set - '+str(full_test_data.isnull().sum().sum()))
     return full_train_data, full_test_data
 
 #%% preprocessing data
 
 def clean_data(full_train_data, full_test_data):
-    # dropping keyword and location columns
-    cols_to_drop = ['keyword','location']
-    full_train_data.drop(cols_to_drop, axis = 1, inplace = True)
-    full_test_data.drop(cols_to_drop, axis = 1, inplace = True)
-
-    # there are no null values in either the train set or the test set.
-    print('Total null values in the train set - '+str(full_train_data.isnull().sum().sum()))
-    print('Total null values in the test set - '+str(full_test_data.isnull().sum().sum()))
-
-    full_train_data['text'] = full_train_data['text'].apply(lambda x: x.lower()) # convert everything to lower case
-    full_test_data['text'] = full_test_data['text'].apply(lambda x: x.lower()) # convert everything to lower case
     
-    for i in range(len(full_train_data['text'])):
+    def tweet_clean(tweet):
+        # convert every tweet to lower case
+        tweet = ''.join([j.lower() for j in tweet])
         # remove website links
-        full_train_data['text'].iloc[i] = re.sub('www.|https://|http://|.com|t.co/','',full_train_data['text'].iloc[i])
-        # remove all punctuation
-        full_train_data['text'].iloc[i] = ''.join([j for j in full_train_data['text'].iloc[i] if j not in string.punctuation])
+        tweet = re.sub('www.|https://|http://|.com|t.co/','',tweet)    
+        # remove all punctuation 
+        tweet = ''.join([j for j in tweet if j not in string.punctuation])    
         # remove all digits
-        full_train_data['text'].iloc[i] = ''.join([j for j in full_train_data['text'].iloc[i] if j not in string.digits])
-        # remove stopwordstest
-        full_train_data['text'].iloc[i] = ' '.join([j for j in full_train_data['text'].iloc[i].split() if j not in stopwords.words('english')])
+        tweet = ''.join([j for j in tweet if j not in string.digits])    
+        # remove stopwords
+        tweet = ' '.join([j for j in tweet.split() if j not in stopwords.words('english')])    
         # remove non ASCII characters
-        full_train_data['text'].iloc[i] = ''.join([j for j in full_train_data['text'].iloc[i] if ord(j) < 128])
+        tweet = ''.join([j for j in tweet if ord(j) < 128])
+        return tweet
+    
+    full_train_data['text'] = full_train_data['text'].apply(lambda x: tweet_clean(x))
+    full_test_data['text'] = full_test_data['text'].apply(lambda x: tweet_clean(x))
+    return full_train_data, full_test_data
 
-    for i in range(len(full_test_data['text'])):
-        # remove website links
-        full_test_data['text'].iloc[i] = re.sub('www.|https://|http://|.com|t.co/','',full_test_data['text'].iloc[i])
-        # remove all punctuation
-        full_test_data['text'].iloc[i] = ''.join([j for j in full_test_data['text'].iloc[i] if j not in string.punctuation])
-        # remove all digits
-        full_test_data['text'].iloc[i] = ''.join([j for j in full_test_data['text'].iloc[i] if j not in string.digits])
-        # remove stopwordstest
-        full_test_data['text'].iloc[i] = ' '.join([j for j in full_test_data['text'].iloc[i].split() if j not in stopwords.words('english')])
-        # remove non ASCII characters
-        full_test_data['text'].iloc[i] = ''.join([j for j in full_test_data['text'].iloc[i] if ord(j) < 128])
+#%% model to be used for tweet analysis
 
-    full_train_data['text'] = full_train_data['text'].apply(lambda x: x.lstrip())   # remove all leading spaces
-    full_train_data['text'] = full_train_data['text'].apply(lambda x: x.rstrip())   # remove all trailing spaces
-    full_test_data['text'] = full_test_data['text'].apply(lambda x: x.lstrip())   # remove all leading spaces
-    full_test_data['text'] = full_test_data['text'].apply(lambda x: x.rstrip())   # remove all trailing spaces
-
-    CV = CountVectorizer()
+def nlp_model(full_train_data, full_test_data):
+    
+    CV = CountVectorizer(analyzer = 'char_wb', ngram_range = (1,5))     # parameters of CV can be played around with
     X = CV.fit_transform(full_train_data['text'])
     y = full_train_data['target']
     X_valid = CV.transform(full_test_data['text'])
+    
+    tfidf = TfidfTransformer()
+    X = tfidf.fit_transform(X)
+    X_valid= tfidf.transform(X_valid)
+    
     return X, y, X_valid
 
 #%% data scaling
@@ -95,16 +88,22 @@ def scaled_data(X, X_valid):
 #%% regressor functions
     
 def regressor_fn_optimised(X, y, X_valid, choice):      
-    from bayes_opt import BayesianOptimization
     
     if choice == 1:                    
+        def regressor_fn(C):            
+            regressor = LogisticRegression(C = C)      
+            cval = cross_val_score(regressor, X, y, scoring = 'balanced_accuracy', cv = 3)
+            return cval.mean()
+        pbounds = {'C': (0.1, 10)}
+        
+    elif choice == 2:                    
         def regressor_fn(alpha):            
             regressor = RidgeClassifier(alpha = alpha)        
-            cval = cross_val_score(regressor, X, y, scoring = 'balanced_accuracy', cv = 5)
+            cval = cross_val_score(regressor, X, y, scoring = 'balanced_accuracy', cv = 3)
             return cval.mean()
-        pbounds = {'alpha': (0, 0)}
+        pbounds = {'alpha': (0.1, 10)}
         
-    elif choice == 2:    
+    elif choice == 3:    
                 
         def regressor_fn(n_neighbors):     
             n_neighbors = int(n_neighbors)
@@ -113,15 +112,15 @@ def regressor_fn_optimised(X, y, X_valid, choice):
             return cval.mean()
         pbounds = {'n_neighbors': (2,10)}
         
-    elif choice == 3:           
+    elif choice == 4:           
         def regressor_fn(n_estimators, max_depth):     
             max_depth, n_estimators = int(max_depth), int(n_estimators)
             regressor = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth)        
-            cval = cross_val_score(regressor, X, y, scoring = 'balanced_accuracy', cv = 5)
+            cval = cross_val_score(regressor, X, y, scoring = 'balanced_accuracy', cv = 5, n_jobs = -1)
             return cval.mean()
         pbounds = {'n_estimators': (10, 500), 'max_depth': (2,20)}
         
-    elif choice == 4: 
+    elif choice == 5: 
         X, X_valid, scaler_X = scaled_data(X, X_valid)      
         def regressor_fn(C, gamma):            
             regressor = SVC(C = C, kernel = 'rbf', gamma = gamma)        
@@ -129,7 +128,7 @@ def regressor_fn_optimised(X, y, X_valid, choice):
             return cval.mean()
         pbounds = {'C': (0.1, 100), 'gamma': (0.01, 100)}
         
-    elif choice == 5:
+    elif choice == 6:
         def regressor_fn(learning_rate, max_depth, n_estimators):            
             max_depth, n_estimators = int(max_depth), int(n_estimators)
             regressor = LGBMClassifier(learning_rate = learning_rate, max_depth = max_depth, n_estimators = n_estimators)        
@@ -146,11 +145,11 @@ def regressor_fn_optimised(X, y, X_valid, choice):
         pbounds = {'learning_rate': (0.01, 1), 'max_depth': (2,50), 'n_estimators': (10, 500)}
     
     optimizer = BayesianOptimization(regressor_fn, pbounds, verbose = 2)
-    optimizer.probe(params = {'alpha':0}, lazy = True)
-    optimizer.maximize(init_points = 5, n_iter = 30)    
+    # optimizer.probe(params = {'learning_rate':1}, lazy = True)
+    optimizer.maximize(init_points = 5, n_iter = 20)    
     # change next line in accordance with choice of regressor made
-    # y_valid_pred = RandomForestClassifier(max_depth = int(optimizer.max['params']['max_depth']), n_estimators = int(optimizer.max['params']['max_depth'])).fit(X, y).predict(X_valid)
-    y_valid_pred = RidgeClassifier().fit(X, y).predict(X_valid)
+    y_valid_pred = LGBMClassifier(learning_rate = optimizer.max['params']['learning_rate'], max_depth = int(optimizer.max['params']['max_depth']), n_estimators = int(optimizer.max['params']['max_depth'])).fit(X, y).predict(X_valid)
+    # y_valid_pred = RidgeClassifier(alpha = optimizer.max['params']['alpha']).fit(X, y).predict(X_valid)
     
     return y_valid_pred, optimizer.max
 
@@ -158,11 +157,11 @@ def regressor_fn_optimised(X, y, X_valid, choice):
 
 if __name__ == '__main__':
     full_train_data, full_test_data = get_data()
-    t1 = time.time()
-    X, y, X_valid = clean_data(full_train_data, full_test_data)
-    t2 = time.time()
-    print(t2-t1)
-    # # y_valid_pred, optimal_params = regressor_fn_optimised(X, y, X_valid, choice = 1)
-    # y_valid_pred = XGBClassifier().fit(X, y).predict(X_valid)
-    # df = pd.DataFrame({'Id':full_test_data.index, 'Target':y_valid_pred})
-    # df.to_csv('prediction.csv', index = False)
+    full_train_data, full_test_data = clean_data(full_train_data, full_test_data)
+    X, y, X_valid = nlp_model(full_train_data, full_test_data)
+    # uncomment the following line if the model's hyper parameters have to be optimised. 
+    y_valid_pred, optimal_params = regressor_fn_optimised(X, y, X_valid, choice = 6)
+    #comment the following line if regressor_fn_optimised is being run
+    # y_valid_pred = LGBMClassifier().fit(X, y).predict(X_valid)
+    df = pd.DataFrame({'Id':full_test_data.index, 'Target':y_valid_pred})
+    df.to_csv('prediction.csv', index = False)
